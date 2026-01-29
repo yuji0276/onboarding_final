3a45e709859d89011067e2ac28630cbcad7acc3d
yuji0276 <yujikami0110@gmail.com>
2026-01-29 17:29:09 +0900

    feat: インポートパスを最新仕様に更新（非推奨な記述を修正）

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,7 +1,6 @@
-#データベースの接続、sessionn管理
+# データベースの接続、sessionn管理
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
