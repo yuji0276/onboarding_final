d1bff28e000108c52a3a00ee6850ef2321c221fd
yuji0276 <yujikami0110@gmail.com>
2026-02-11 02:02:15 +0900

    refactor:dbの取得を安全に実装

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -14,9 +14,12 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-def get_db():
+
+def get_db_session():
+    db = None
     try:
         db = SessionLocal()
         yield db
     finally:
-        db.close()
+        if db is not None:
+            db.close()
