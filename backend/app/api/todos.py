abd2c6b2c27589b39dbdbf631b7cb17e72bf6af1
yuji0276 <yujikami0110@gmail.com>
2026-02-11 01:48:37 +0900

    refactor:delete時の返り値をNoneからboolに変更
    成功可否を返すことでエラーハンドリングが可能になる

--- backend/app/api/todos.py
+++ backend/app/api/todos.py
@@ -1,5 +1,5 @@
 from app.schemas.todo import TodoPost, TodoGet, TodoUpdate
-from app.core.database import get_db
+from app.core.database import get_db_session
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, HTTPException, Depends
 from app.crud.get_todos import get_all_todos, get_todo
@@ -11,17 +11,17 @@ router = APIRouter()
 
 
 @router.get("/api/todos", response_model=list[TodoGet], status_code=200)
-def get_todos(db: Session = Depends(get_db)):
+def get_todos(db: Session = Depends(get_db_session)):
     return get_all_todos(db)
 
 
 @router.post("/api/todos", response_model=TodoGet, status_code=201)
-def create_new_todo(todo: TodoPost, db: Session = Depends(get_db)):
+def create_new_todo(todo: TodoPost, db: Session = Depends(get_db_session)):
     return create_todo(todo, db)
 
 
 @router.get("/api/todos/{id}", response_model=TodoGet, status_code=200)
-def read_todo(id: int, db: Session = Depends(get_db)) -> TodoGet | None:
+def read_todo(id: int, db: Session = Depends(get_db_session)) -> TodoGet | None:
     todo = get_todo(id, db)
 
     if todo is None:
@@ -30,12 +30,17 @@ def read_todo(id: int, db: Session = Depends(get_db)) -> TodoGet | None:
 
 
 @router.delete("/api/todos/{id}", status_code=204)
-def delete_todo_item(id: int, db: Session = Depends(get_db)):
-    deleted_todo = delete_todo(id, db)
-    return deleted_todo
+def delete_todo_item(id: int, db: Session = Depends(get_db_session)):
+    is_delete_success = delete_todo(id, db)
+
+    if is_delete_success:
+        raise HTTPException(status_code=404, detail="todo not found")
+    return None
 
 
-@router.put("/api/todos/{id}", response_model=TodoGet, status_code=200)
-def update_todo_item(id: int, todo: TodoUpdate, db: Session = Depends(get_db)):
-    updated_todo = update_todo(id, todo, db)
+@router.patch("api/todos/{id}", response_model=TodoGet, status_code=200)
+def update_todo(id: int, todo: TodoUpdate, db: Session = Depends(get_db_session)):
+    updated_todo = update_todo(id, todo, db=Depends(get_db_session))
+    if updated_todo is None:
+        raise HTTPException(status_code=404, detail="todo not found")
     return updated_todo
--- backend/app/crud/delete_todo.py
+++ backend/app/crud/delete_todo.py
@@ -1,13 +1,13 @@
 from app.models.todo import Todo
+from app.schemas.todo import TodoGet
 from sqlalchemy.orm import Session
 
 
-def delete_todo(todo_id: int, db: Session) -> None:
-    db_todo = db.get(Todo, todo_id)
-    if not db_todo:
-        print("Todo not found")
-        return None
-    db.delete(db_todo)
+def delete_todo(todo_id: int, db: Session) -> bool:  # idを指定して成功可否のみを返す
+    deleted_todo = None
+    deleted_todo = db.get(Todo, todo_id)
+    if deleted_todo is None:
+        return False
+    db.delete(deleted_todo)
     db.commit()
-    print("Todo deleted successfully")
-    return None
+    return True
